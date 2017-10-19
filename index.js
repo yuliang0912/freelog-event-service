71d4fddb9ddc0f472a2b1870bef8975ef7704727
yuliang <yu.liang@freelog.com>
2017-10-19 10:07:38 +0800

    1.修改了egg-wokrer启动进程数量

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ global.Promise = require('bluebird')
 require('egg').startCluster({
     baseDir: __dirname,
     port: process.env.PORT || 7010, // default to 7001
-    workers: 1
+    workers: 2
 });
